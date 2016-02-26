8f2a9400652a78be8c6746d0b8999b81eed62904
benser1 <briganenser28@gmail.com>
2016-02-26 10:35:04 -0700

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,41 @@
 function getFarenheits(result){
-  // Your code goes here
+  temps = [];
+  $.each(result.hourly_forecast, function(){
+    temps.push(this.temp.english);
+  });
+  return temps;
 }
 
 function getHours(result){
-  // Your code goes here
+  hours = []
+  $.each(result.hourly_forecast, function(){
+    hours.push(this.FCTTIME.hour);
+  })
+  return hours;
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  var weatherData = {};
+  weatherData.labels = labels;
+  weatherData.datasets = [
+    {
+      label: "Hourly Weather for New York",
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: data
+    }
+  ]
+      return weatherData;
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax ({
+    url: endpoint,
+    dataType: 'jsonp',
+    success: success
+  });
 }
