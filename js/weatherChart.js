3025115336ea0215a85b23b0efb4f1108372fe16
jakebrady5 <jakebrady5@gmail.com>
2016-02-27 21:35:03 -0500

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,40 @@
 function getFarenheits(result){
-  // Your code goes here
+  var degrees = [];
+  result.hourly_forecast.forEach(function(forecast){
+    degrees.push(forecast.temp.english);
+  });
+  return degrees;
 }
 
 function getHours(result){
-  // Your code goes here
+  var hours = [];
+  result.hourly_forecast.forEach(function(forecast){
+    hours.push(forecast.FCTTIME.hour);
+  });
+  return hours;
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  var dataset = {
+    labels: labels,
+    datasets: [{
+      label : 'Hourly Weather for New York',
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: data
+    }]
+  };
+  return dataset;
 }
 
-function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+function makeAjaxRequest(endpoint, callback) {
+  $.ajax({
+    url: endpoint,
+    dataType: 'jsonp',
+    success: callback
+  });
 }
