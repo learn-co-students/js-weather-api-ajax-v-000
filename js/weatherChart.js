e6d9b1bcaab1d9b7fe3a0d3cbd4fce2d37614ffd
macgeek30 <mike25ko@gmail.com>
2016-03-19 14:39:40 -0600

    Done

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,39 @@
 function getFarenheits(result){
-  // Your code goes here
+  return $.map(result.hourly_forecast,function(hour){
+    return hour.temp.english;
+  });
 }
 
 function getHours(result){
-  // Your code goes here
+  return $.map(result.hourly_forecast,function(hour){
+    return hour.FCTTIME.hour;
+  });
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  
+  var data = {
+      labels: labels,
+      datasets: [
+          {
+              label: "Hourly Weather for New York",
+              fillColor: "rgba(220,220,220,0.2)",
+              strokeColor: "rgba(220,220,220,1)",
+              pointColor: "rgba(220,220,220,1)",
+              pointStrokeColor: "#fff",
+              pointHighlightFill: "#fff",
+              pointHighlightStroke: "rgba(220,220,220,1)",
+              data: data
+          },
+      ]
+  }
+  return data;
 }
 
-function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+function makeAjaxRequest(url, success) {
+  $.ajax({
+    url:url,
+    dataType: 'jsonp',
+    success: success
+  });
 }
