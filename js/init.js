f24cd1063d34066221e4be99d6ba00a7add9b224
febbraiod <donedesign1@gmail.com>
2016-04-22 13:45:59 -0400

    Done.

--- js/init.js
+++ js/init.js
@@ -1,10 +1,11 @@
 jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
+  var API_KEY = "96d16a406b933e3e";
   var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
   makeAjaxRequest(URL, function(json) {
     var data = generateDataSet(getHours(json), getFarenheits(json));
-    var tempChart = new Chart(ctx).Line(data, { bezierCurve: true})
+    var tempChart = new Chart(ctx).Line(data, { bezierCurve: true});
   });
+
 });
