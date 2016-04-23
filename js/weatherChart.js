e1f01e9bf0f504e9f62cd2387710e8a70b45526c
jsgoldb <jgoldb@yahoo.com>
2016-04-22 19:45:47 -0500

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,53 @@
 function getFarenheits(result){
   // Your code goes here
+  degrees = $.map(result.hourly_forecast, function(hour, index){
+    return hour['temp']['english'];
+  })
+  return degrees;
 }
 
 function getHours(result){
   // Your code goes here
+  hours = $.map(result.hourly_forecast, function(hour, index){
+    return hour['FCTTIME']['hour'];
+  })
+  return hours;
 }
 
 function generateDataSet(labels, data) {
   // Your code goes here
+
+  var dataToChart = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Hourly Weather for New York",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: data
+      }
+    ]
+  };
+  return dataToChart;
+
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+
+  // $.getJSON(endpoint + '?callback=?', function(response) {
+  //   success(response);
+  // })
+  $.ajax({
+    url: endpoint,
+    dataType: 'jsonp',
+    success: function onSuccess(data){
+      success(data);
+    }
+  })
+
+  
 }
