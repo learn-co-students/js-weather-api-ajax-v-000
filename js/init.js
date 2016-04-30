9a5b9a46e6757da240df818e2cf55a72f2c4ae42
authorbeard <msnolan@gmail.com>
2016-04-30 13:49:32 -0400

    ajax call working, chart displays

--- js/init.js
+++ js/init.js
@@ -1,10 +1,11 @@
 jQuery(document).ready(function($) {
-  var API_KEY = mySecret;
+  var API_KEY = mySecret();
   var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
   makeAjaxRequest(URL, function(json) {
-    var data = generateDataSet(getHours(json), getFarenheits(json));
+    var parsedJson = json["hourly_forecast"]
+    var data = generateDataSet(getHours(parsedJson), getFarenheits(parsedJson));
     var tempChart = new Chart(ctx).Line(data, { bezierCurve: true})
   });
 });
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,39 @@
 function getFarenheits(result){
+  // debugger;
   // Your code goes here
+  var tempArray = $.map(result, function(temp, index) {
+    return temp.temp.english
+  })
+  return tempArray
 }
 
 function getHours(result){
   // Your code goes here
+  var hoursArray = $.map(result, function(hour, index) {
+    return hour.FCTTIME.hour 
+  })
+  return hoursArray
 }
 
 function generateDataSet(labels, data) {
   // Your code goes here
-}
+  var data = {
+    labels: labels,
+    datasets: [
+        {
+            label: "NYC hourly forecast",
+            fillColor: 'rgba(220,220,220,0.5)',
+            strokeColor: 'rgba(220,220,220,0.8)',
+            highlightFill: 'rgba(220,220,220,0.75)',
+            highlightStroke: 'rgba(220,220,220,1)',
+            data: data,
+        }
+      ]
+    }
+  return data  
+};
+
 
 function makeAjaxRequest(endpoint, success) {
-  $.get"
+  $.get(endpoint, success)
 }
