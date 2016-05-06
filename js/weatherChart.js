cd0eb20c3d938fd00d0087e2f2ccc33a82b14677
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-05-06 17:15:30 -0500

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,46 @@
 function getFarenheits(result){
   // Your code goes here
+  var farenheit = [];
+  for(var i = 0; i < result["hourly_forecast"].length; i++){
+    farenheit.push(result["hourly_forecast"][i]["temp"]["english"]);
+  }
+  return farenheit;
 }
 
 function getHours(result){
   // Your code goes here
+  var hours = [];
+  for(var i = 0; i < result["hourly_forecast"].length;i++){
+    hours.push(result["hourly_forecast"][i]["FCTTIME"]["hour"]);
+  }
+  return hours;
 }
 
 function generateDataSet(labels, data) {
   // Your code goes here
+  var dataSet = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Hourly Weather for New York",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: data
+      }
+    ]
+  };
+  return dataSet;
 }
 
 function makeAjaxRequest(endpoint, success) {
   // Your code goes here
+  $.ajax({
+    url: endpoint,
+    dataType: 'jsonp',
+    success: success
+  });
 }
