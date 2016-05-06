0b68152d4de6b3cfeaeb72bac2895f95c113c174
rocketNeck <homestarflyer@gmail.com>
2016-05-06 16:55:19 -0400

    Done.

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
+  var API_KEY = "cc376e2f35ca52d6";
   var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
@@ -8,3 +8,5 @@ jQuery(document).ready(function($) {
     var tempChart = new Chart(ctx).Line(data, { bezierCurve: true})
   });
 });
+
+//"http://api.wunderground.com/api/cc376e2f35ca52d6/hourly/q/NY/New_York.json"
