1618206044572a26a69151aec16823a55e30ee43
NStephenson <nickstephenson90@gmail.com>
2016-05-09 15:34:41 -0400

    made all tests pass

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,39 @@
 function getFarenheits(result){
-  // Your code goes here
+  var farenheits = [];
+  result['hourly_forecast'].forEach(function(hourly){
+    farenheits.push(hourly['temp']['english']);
+  });
+  return farenheits;
 }
 
 function getHours(result){
-  // Your code goes here
+  var hours = [];
+  result['hourly_forecast'].forEach(function(hourly){
+    hours.push(hourly['FCTTIME']['hour']);
+  });
+  return hours;
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  return {
+    labels: labels,
+    datasets: [ { 
+      label : 'Hourly Weather for New York', 
+      fillColor : 'rgba(220,220,220,0.2)', 
+      strokeColor : 'rgba(220,220,220,1)', 
+      pointColor : 'rgba(220,220,220,1)', 
+      pointStrokeColor : '#fff', 
+      pointHighlightFill : '#fff', 
+      pointHighlightStroke : 'rgba(220,220,220,1)',
+      data : data 
+    } ]
+  }
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({
+    url: endpoint,
+    dataType: 'jsonp',
+    success: success
+  });
 }
