aed945745cfd632a19143f42d8ef9726c8ee9caa
mirandalily <mirandaraymond@me.com>
2016-06-28 14:50:50 -0700

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,42 @@
 function getFarenheits(result){
-  // Your code goes here
+  var temps = [];
+  $.each(result['hourly_forecast'], function(index, data) {
+    temps.push(data.temp.english);
+  })
+  return temps;
 }
 
 function getHours(result){
-  // Your code goes here
+  var hours = [];
+  $.each(result['hourly_forecast'], function(index, data) {
+    hours.push(data.FCTTIME.hour);
+  })
+  return hours;
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  var dataSet= {
+    labels: labels,
+    datasets: [
+      {
+        label: "Hourly Weather for New York",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: data,
+      }
+    ]
+  };
+  return dataSet;
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({
+    url: endpoint,
+    dataType: 'jsonp',
+    success: success
+  })
 }
