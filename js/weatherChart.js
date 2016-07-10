1f0850f238098e39728aae1bab3fcab707fe4f0e
thebrianemory <the@brianemory.com>
2016-07-10 15:49:49 -0700

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,44 @@
 function getFarenheits(result){
   // Your code goes here
+  temps = [];
+  for (var i = 0; i < result.hourly_forecast.length; i++) {
+    temps.push(result.hourly_forecast[i].temp.english);
+  }
+  return temps;
 }
 
 function getHours(result){
   // Your code goes here
+  hours = [];
+  for (var i = 0; i < result.hourly_forecast.length; i++) {
+    hours.push(result.hourly_forecast[i].FCTTIME.hour);
+  }
+  return hours;
 }
 
-function generateDataSet(labels, data) {
-  // Your code goes here
+function generateDataSet(times, temps) {
+  var data = {
+    labels: times,
+    datasets: [
+        {
+            label : 'Hourly Weather for New York',
+            fillColor : 'rgba(220,220,220,0.2)',
+            strokeColor : 'rgba(220,220,220,1)',
+            pointColor : 'rgba(220,220,220,1)',
+            pointStrokeColor : '#fff',
+            pointHighlightFill : '#fff',
+            pointHighlightStroke : 'rgba(220,220,220,1)',
+            data: temps
+        }
+    ]
+};
+  return data;
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({
+    url : endpoint,
+    dataType : "jsonp",
+    success : success
+  });
 }
