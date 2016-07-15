0e53c74ae027d55346d18e0625891a80b7ab31ec
Minh <mhnguyen289@gmail.com>
2016-07-15 13:22:48 -0700

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,39 @@
 function getFarenheits(result){
-  // Your code goes here
+    fs = []
+  $.each(result.hourly_forecast, function(index, forecast) {
+    fs.push(forecast.temp.english);
+  });
+  return fs;
+
 }
 
 function getHours(result){
-  // Your code goes here
+    fs = []
+  $.each(result.hourly_forecast, function(index, forecast) {
+    fs.push(forecast.FCTTIME.hour);
+  });
+  return fs;
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+    return {
+    labels: labels,
+    datasets: [
+    {
+      label: "Hourly Weather for New York",
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: data
+    }
+    ]
+  };
+
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+    $.ajax({ url : endpoint, dataType : "jsonp", success: success})
 }
