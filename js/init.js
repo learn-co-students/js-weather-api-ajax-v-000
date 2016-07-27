1d8d4ef163447cd14ea469eccdbb6a9515e46048
cassaram09 <cassaram09@gmail.com>
2016-07-27 17:50:50 -0400

    require jQuery, Chart, weatherChart, and set API key

--- js/init.js
+++ js/init.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
-  var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
+  var API_KEY = "6df88e8936f25d17";
+  var URL = "http://api.wunderground.com/api/" + "6df88e8936f25d17" + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
   makeAjaxRequest(URL, function(json) {
