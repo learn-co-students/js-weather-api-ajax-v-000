f5bcd08bce207233fa9b75c1f51a0da7f2490586
cassaram09 <cassaram09@gmail.com>
2016-07-27 18:32:14 -0400

    Done.

--- js/init.js
+++ js/init.js
@@ -1,10 +1,11 @@
 jQuery(document).ready(function($) {
   var API_KEY = "6df88e8936f25d17";
-  var URL = "http://api.wunderground.com/api/" + "6df88e8936f25d17" + "/hourly/q/NY/New_York.json";
-  var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
+  var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
+  var ctx = document.getElementById("NYCWeatherChart").getContext("2d");
 
   makeAjaxRequest(URL, function(json) {
     var data = generateDataSet(getHours(json), getFarenheits(json));
     var tempChart = new Chart.Line(ctx, {data: data, options: { bezierCurve: true}});
+    new Chart(ctx).Bar(data)
   });
 });
