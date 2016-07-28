8a61eee77398f5df895a932abf855d4b04617cc4
theRingleman <sam.ringleman@gmail.com>
2016-07-28 13:20:05 -0600

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,40 @@
 function getFarenheits(result){
-  // Your code goes here
+  var temperatures = [];
+  $.each(result.hourly_forecast, function(index, forecast){
+    temperatures.push(forecast.temp.english);
+  });
+  return temperatures;
 }
 
 function getHours(result){
-  // Your code goes here
+  var hours = [];
+  $.each(result.hourly_forecast, function(index, forecast){
+    hours.push(forecast.FCTTIME.hour);
+  });
+  return hours;
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  var data = {
+    labels: labels,
+    datasets: [{
+        data: data,
+        label: "Hourly Weather for New York",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)"
+    }]
+  }
+  return data;
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({
+    url: endpoint,
+    dataType: 'jsonp',
+    success: success
+  });
 }
