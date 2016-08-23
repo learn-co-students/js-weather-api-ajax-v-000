b171e9ac5e9bee24094d8b95af64a8bdd07a3fe0
JENNIFER YAMADA <jenny.yamada@gmail.com>
2016-08-22 17:48:42 -0700

    done

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,40 @@
 function getFarenheits(result){
-  // Your code goes here
+  temps = [];
+  result.hourly_forecast.forEach(function(i){
+    temps.push(i.temp.english);
+  });
+  return temps;
 }
 
 function getHours(result){
-  // Your code goes here
+  return result.hourly_forecast.map(function(i) {
+    return i.FCTTIME.hour
+  });
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  var chartData = {
+    labels: labels,
+      datasets: [
+          {
+            label : 'Hourly Weather for New York',
+            fillColor : 'rgba(220,220,220,0.2)',
+            strokeColor : 'rgba(220,220,220,1)',
+            pointColor : 'rgba(220,220,220,1)',
+            pointStrokeColor : '#fff',
+            pointHighlightFill : '#fff',
+            pointHighlightStroke : 'rgba(220,220,220,1)',
+            data: data
+          }
+      ]
+    };
+    return chartData;
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({
+    url: endpoint,
+    dataType : "jsonp",
+    success: success
+  });
 }
