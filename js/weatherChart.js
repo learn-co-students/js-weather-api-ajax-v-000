5f0f335724af70eadc59aa4c4d225a5e4ead13d9
Adam Taitano <adam.taitano@gmail.com>
2016-09-12 17:26:15 +0000

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,43 @@
-function getFarenheits(result){
-  // Your code goes here
-}
-
 function getHours(result){
-  // Your code goes here
-}
+  var data = result["hourly_forecast"];
+  var hours = [];
+  for (var i = 0; i < data.length; i++) {
+    hours.push(data[i]["FCTTIME"]["hour"])
+  }
+  return hours;
+};
+
+function getFarenheits(result){
+  var data = result["hourly_forecast"];
+  var temperature = [];
+  for (var i = 0; i < data.length; i++) {
+    temperature.push(data[i]["temp"]["english"])
+  }
+  return temperature
+};
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  var dataObj = {};
+  dataObj.labels = labels;
+  dataObj.datasets = [
+    {
+      label: "Hourly Weather for New York",
+      fillColor : 'rgba(220,220,220,0.2)',
+      strokeColor : 'rgba(220,220,220,1)',
+      pointColor : 'rgba(220,220,220,1)',
+      pointStrokeColor : '#fff',
+      pointHighlightFill : '#fff',
+      pointHighlightStroke : 'rgba(220,220,220,1)'
+      data: data
+    }
+  ];
+  return dataObj;
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({
+    url: endpoint,
+    dataType: 'jsonp',
+    success: success
+  })
 }
