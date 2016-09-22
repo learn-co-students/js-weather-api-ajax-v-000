0700a826bb426e0503f45c240e9e7f59cdbfc655
sensei100 <jenanton01@gmail.com>
2016-09-22 09:17:50 -0400

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,41 @@
+var dataSetProperties = {
+  label: "Hourly Weather for New York",
+  fillColor : 'rgba(220,220,220,0.2)',
+  strokeColor : 'rgba(220,220,220,1)',
+  pointColor : 'rgba(220,220,220,1)',
+  pointStrokeColor : '#fff',
+  pointHighlightFill : '#fff',
+  pointHighlightStroke : 'rgba(220,220,220,1)'
+};
+
 function getFarenheits(result){
-  // Your code goes here
+  var temp = [];
+  for (var i = 0; i < result.hourly_forecast.length; i++) {
+    temp.push(result.hourly_forecast[i].temp.english);
+  }
+  return temp;
 }
 
 function getHours(result){
-  // Your code goes here
+  var hours = [];
+  for (var i = 0; i < result.hourly_forecast.length; i++) {
+    hours.push(result.hourly_forecast[i].FCTTIME.hour);
+  }
+  return hours;
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  var chartData = {};
+    chartData.labels = labels;
+    chartData.datasets = [dataSetProperties];
+    chartData.datasets[0].data = data;
+    return chartData;
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({
+    url: endpoint,
+    dataType: "jsonp",
+    success: success
+  });
 }
