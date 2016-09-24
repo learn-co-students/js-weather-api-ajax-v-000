e9b4129cdb47eda0b9533d8d0ded804f74831347
arturusfury <bri.reynolds@gmail.com>
2016-09-23 21:48:16 -0500

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,39 @@
 function getFarenheits(result){
-  // Your code goes here
+  var temps = [];
+  for (i = 0;i < result.hourly_forecast.length; i++)
+  {
+    temps.push(result.hourly_forecast[i].temp.english)
+  }
+  return temps;
 }
 
 function getHours(result){
-  // Your code goes here
+  var hours = [];
+  for (i = 0;i < result.hourly_forecast.length; i++)
+  {
+    hours.push(result.hourly_forecast[i].FCTTIME.hour_padded)
+  }
+  return hours;
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  return {
+      labels: labels,
+      datasets: [
+      {
+        label: "Hourly Weather for New York",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: data
+      }
+      ]
+    };
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({url: endpoint, dataType: 'jsonp', success: success})
 }
