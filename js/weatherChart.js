cf62217c40022618ebd96e693a6e2e0579dde5e2
Jesse Novotny <jessenovotny@gmail.com>
2016-10-12 17:55:15 -0700

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,37 @@
-function getFahrenheits(result){
-  // Your code goes here
+function getFahrenheits(result) {
+  return result.hourly_forecast.map(function(item) {
+    return item.temp.english;
+  });
 }
 
-function getHours(result){
-  // Your code goes here
+function getHours(result) {
+  return result.hourly_forecast.map(function(item) {
+    return item.FCTTIME.hour;
+  });
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  return {
+    labels: labels,
+    datasets: [
+    {
+      label: "Hourly Weather for New York",
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: data
+    }
+    ]
+  };
 }
 
-function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+function makeAjaxRequest(url, result) {
+  $.ajax({ 
+    url : url, 
+    dataType : "jsonp", 
+    success: result
+  });
 }
