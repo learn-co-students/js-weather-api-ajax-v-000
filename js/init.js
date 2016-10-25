17a09b27d9288a72f011230b81131edb40d1dba0
Kailee <kaileeagray@gmail.com>
2016-10-24 19:37:33 -0500

    Done.

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
+  var API_KEY = "1e034522e5ad16a7";
   var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
