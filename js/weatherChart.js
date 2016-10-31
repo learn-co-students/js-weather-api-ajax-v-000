59cb3a6860d6f8dd1b6d8d55f490987049786d66
jbok4 <jbok4@aol.com>
2016-10-31 17:04:07 -0400

    working, almost done but untested

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,33 @@
 function getFahrenheits(result){
-  // Your code goes here
+  return result.hourly_forecast.map(function(item) {
+    return item.temp.english;
+  })
 }
 
 function getHours(result){
-  // Your code goes here
+  return result.hourly_forecast.map(function(item) {
+    return item.FCTTIME.hour;
+  })
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+ var generateData;
+
+  labels: labels,
+  datasets: [{
+    label: 'Hourly Weather for New York',
+    fillColor: 'rgba(220,220,220,0.5)',
+    strokeColor: 'rgba(220,220,220,0.8)',
+    pointColor: 'rgba(220,220,220,1)',
+    pointStrokeColor: '#fff',
+    pointHighlightFill: '#fff',
+    highlightStroke: 'rgba(220,220,220,1)',
+    data: data
+  }]
+};
+  return generateData;
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({ url : endpoint, dataType : "jsonp", success: success})
 }
