806f99e5f66731c874bdb849a00c5b07b9908a5f
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-11-03 18:32:07 -0700

    Done.

--- js/init.js
+++ js/init.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
-  var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
+  var API_KEY = "96fecc79325e0a25";
+  var URL = "http://api.wunderground.com/api/" + "96fecc79325e0a25" + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
   makeAjaxRequest(URL, function(json) {
@@ -8,3 +8,6 @@ jQuery(document).ready(function($) {
     var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
   });
 });
+
+
+
