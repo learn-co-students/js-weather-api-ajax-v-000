05764d49f077cf66836bf464910a0a39b1cb6251
Logan McAnsh <l.m@loganmcansh.com>
2016-11-05 00:29:59 -0400

    Done.

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
-jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
+$(document).ready(function($) {
+  var API_KEY = "1c8d6b68aaf955fe";
   var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
