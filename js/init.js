6b3f2261b58e84a75287d0954324db25b37222c3
lenn hypolite <lennhydesign@gmail.com>
2016-11-16 15:46:08 -0500

    Done.

--- js/init.js
+++ js/init.js
@@ -2,10 +2,10 @@ jQuery(document).ready(function($) {
   var API_KEY = "b41b1233d35aa647";
   var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").getContext("2d");
-  new Chart(ctx).Line(data, { bezierCurve: true });
 
   makeAjaxRequest(URL, function(json) { // json is the object from the ajax call and this anonymous function is the succes function
     var data = generateDataSet(getHours(json), getFahrenheits(json));
+    new Chart(ctx).Line(data);
 
   });
 });
