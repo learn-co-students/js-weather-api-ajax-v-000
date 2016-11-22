cc15fad5512afa8f7f10bd2d27efc1d3fc7ea00f
dannyenfp <daniel.valmas@gmail.com>
2016-11-22 18:30:01 -0500

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,42 @@
 function getFahrenheits(result){
-  // Your code goes here
+  console.log(result);
+  fahr = [];
+  for (var i = 0; i < result.hourly_forecast.length; i++) {
+    fahr.push( result.hourly_forecast[i].temp.english )
+  }
+  return fahr
 }
 
 function getHours(result){
-  // Your code goes here
+  h = [];
+  for (var i = 0; i < result.hourly_forecast.length; i++) {
+    h.push(result.hourly_forecast[i].FCTTIME.hour);
+  }
+  return h;
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+   return {
+    labels: labels,
+    datasets: [
+    {
+      label: "Hourly Weather for New York",
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: data
+    }
+    ]
+  };
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+   $.ajax( {
+    url: endpoint,
+    dataType: 'jsonp',
+    success: success
+  } );
 }
