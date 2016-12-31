06a90f72f566593fdd4b7f457915765d94b00440
Clare <clare.e.horton@gmail.com>
2016-12-31 00:06:23 -0600

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,47 @@
+var json = "";
+
 function getFahrenheits(result){
-  // Your code goes here
+  var forecasts = result.hourly_forecast;
+  var temps = [];
+  for (var i=0; i < forecasts.length; i++) {
+    temps.push(forecasts[i].temp.english);
+  };
+  return temps;
 }
 
 function getHours(result){
-  // Your code goes here
+  var forecasts = result.hourly_forecast;
+  var hours = [];
+  for (var i=0; i < forecasts.length; i++) {
+    hours.push(forecasts[i].FCTTIME.hour);
+  };
+  return hours;
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  var chartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Hourly Weather for New York",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+      }
+    ]
+  };
+
+  chartData.datasets[0].data = data;
+  return chartData;
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({
+    url: endpoint,
+    dataType: 'jsonp',
+    success: success
+  })
 }
