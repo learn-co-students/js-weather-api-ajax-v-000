0abb8316589c076625ed46cc7df429ad32606770
ldgarber <leah.garber88@gmail.com>
2017-01-02 12:49:00 -0500

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,39 @@
 function getFahrenheits(result){
   // Your code goes here
+  var temps = []; 
+  result.hourly_forecast.forEach( function(forecast){
+    temps.push(forecast.temp.english);  
+  }); 
+  return temps; 
 }
 
 function getHours(result){
   // Your code goes here
+  var hours = []; 
+  result.hourly_forecast.forEach( function(forecast){
+    hours.push(forecast.FCTTIME.hour); 
+  }); 
+  return hours; 
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  var dataObj = {}; 
+  dataObj.labels = labels; 
+  dataObj.datasets = []; 
+  var dataSet = {
+    label: "Hourly Weather for New York",
+    fillColor: "rgba(220,220,220,0.2)",
+    strokeColor: "rgba(220,220,220,1)",
+    pointColor: "rgba(220,220,220,1)",
+    pointStrokeColor: "#fff",
+    pointHighlightFill: "#fff",
+    pointHighlightStroke: "rgba(220,220,220,1)",
+    data: data
+  }; 
+  dataObj.datasets.push(dataSet); 
+  return dataObj; 
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({url: endpoint, dataType: "jsonp", success: success });
 }
