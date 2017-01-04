378f29e0ab782714284d2df7c41a9f52221776cc
AliceKB <alicekristinabalbuena@gmail.com>
2017-01-03 19:24:23 -0500

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,43 @@
 function getFahrenheits(result){
   // Your code goes here
+  var temps = []
+  $.each(result.hourly_forecast, function(index, forecast) {
+    temps.push(forecast.temp.english)
+  })
+  return temps;
 }
 
 function getHours(result){
   // Your code goes here
+  var hours = []
+  $.each(result.hourly_forecast, function(index, forecast) {
+    hours.push(forecast.FCTTIME.hour)
+  })
+  return hours;
 }
 
 function generateDataSet(labels, data) {
   // Your code goes here
+  var dataObj = {};
+  dataObj.labels = labels;
+  dataObj.datasets = [{
+    label : 'Hourly Weather for New York',
+    fillColor: 'rgba(220,220,220,0.2)',
+    strokeColor: 'rgba(220,220,220,1)',
+    pointColor : 'rgba(220,220,220,1)',
+    pointStrokeColor : '#fff',
+    pointHighlightFill : '#fff',
+    pointHighlightStroke : 'rgba(220,220,220,1)',
+    data: data
+  }];
+  return dataObj;
 }
 
 function makeAjaxRequest(endpoint, success) {
   // Your code goes here
+  $.ajax({
+    url: endpoint,
+    dataType : 'jsonp',
+    success: success
+  })
 }
