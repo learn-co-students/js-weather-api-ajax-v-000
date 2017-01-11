7045ecfd6f35a494bc6026d6d942f0538bee23e2
MIKE-RANDELL <michael.james.randell@gmail.com>
2017-01-11 10:47:18 -0500

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,18 +1,40 @@
 function getFahrenheits(result){
-  // Your code goes here
+  var temp_array = [];
+  $.each(result.hourly_forecast, function(index, hourly_obj){
+    temp_array.push(hourly_obj.temp.english)
+  });
+  return temp_array;
 }
 
 function getHours(result){
-  // Your code goes here
+  var hour_array = [];
+  $.each(result.hourly_forecast, function(index, hourly_obj){
+    hour_array.push(hourly_obj.FCTTIME.hour)
+  });
+  return hour_array;
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  var dataObj = {};
+  dataObj.labels = labels;
+  dataObj.datasets = [
+    {
+      label : 'Hourly Weather for New York',
+      fillColor : 'rgba(220,220,220,0.2)',
+      strokeColor : 'rgba(220,220,220,1)',
+      pointColor : 'rgba(220,220,220,1)',
+      pointStrokeColor : '#fff',
+      pointHighlightFill : '#fff',
+      pointHighlightStroke : 'rgba(220,220,220,1)',
+      data: data
+    }
+  ]
+  return dataObj;
 }
 
 function makeAjaxRequest(endpoint, success) {
-  debugger;
-  $.ajax({
+
+  var obj = $.ajax({
     url: endpoint,
     dataType: 'jsonp',
     success: success,
