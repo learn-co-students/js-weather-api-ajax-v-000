62d78eed0209e114bc2205f5feadf1fcf6dd6a90
bmolina-nyc <bruce.molina.81@gmail.com>
2017-01-31 20:50:21 -0500

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,23 +1,61 @@
 function getFahrenheits(result){
   // Your code goes here
+  return result['hourly_forecast'].map(function(object){
+    return object['temp'].english
+  })
 }
 
 function getHours(result){
   // Your code goes here
+  return result['hourly_forecast'].map(function(object){
+    return object['FCTTIME'].hour
+  });
 }
 
 function generateDataSet(labels, data) {
   // Your code goes here
-}
+  return {
+    labels: labels,
+    datasets: [
+      {
+         label: "Hourly Weather for New York",
+          fillColor: "rgba(220,220,220,0.2)",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: data
+      }
+    ]
+  };
+  }
+
 
 function makeAjaxRequest(endpoint, success) {
   // Your code goes here
   $.ajax({
     url: endpoint,
-    datatype: 'jsonp',
-    success: function(success){
-      console.log(success);
-    }
+    dataType: 'jsonp',
+    success:  success
   });
 }
 
+ // it("generates the right dataset for chart.js", function(){
+ //    expected = {
+ //      labels: ["label1", "label2"],
+ //      datasets: [
+ //      {
+ //        label: "Hourly Weather for New York",
+ //        fillColor: "rgba(220,220,220,0.2)",
+ //        strokeColor: "rgba(220,220,220,1)",
+ //        pointColor: "rgba(220,220,220,1)",
+ //        pointStrokeColor: "#fff",
+ //        pointHighlightFill: "#fff",
+ //        pointHighlightStroke: "rgba(220,220,220,1)",
+ //        data: ["data1", "data2"]
+ //      }
+ //      ]
+ //    };
+ //    expect(generateDataSet(["label1", "label2"], ["data1", "data2"])).toEqual(expected);
+ //  })
