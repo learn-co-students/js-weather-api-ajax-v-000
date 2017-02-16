9e73cb0e264c9c7185ea0d762db423d512c04caf
lcorr8 <l.corr.v@gmail.com>
2017-02-16 15:55:36 +0100

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,48 @@
 function getFahrenheits(result){
-  // Your code goes here
+  //hourly_forecast[0].temp.english
+  //var hourly_result = result.hourly_forecast
+  //var temp = []
+  //$.each(hourly_result, function(index, hourly) {
+    //temp.push(hourly.temp.english)
+  //})
+  //return temp
+  return result.hourly_forecast.map(function(item){
+    return item.temp.english
+  })
 }
 
 function getHours(result){
-  // Your code goes here
+  //hourly_forecast[0].FCTTIME.hour
+  return result.hourly_forecast.map(function(item){
+    return item.FCTTIME.hour
+  })
 }
 
 function generateDataSet(labels, data) {
   // Your code goes here
+  return {
+    labels: labels,
+    datasets: [
+    {
+      label: "Hourly Weather for New York",
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: data
+    }
+    ]
+  };
+  return data
 }
 
 function makeAjaxRequest(endpoint, success) {
   // Your code goes here
+  $.ajax({
+    url: endpoint,
+    dataType: "jsonp",
+    success: success
+  })
 }
